030fe7ea3ebb94a2c7542bba5b8e00bd5c67a2c0
shaquinbale <scopelenskingdra@gmail.com>
2024-10-14 11:38:41 -0400

    Add win deciding function

--- scripts.js
+++ scripts.js
@@ -2,19 +2,31 @@ let humanScore = 0
 let computerScore = 0
 
 function getComputerChoice() {
-    const intChoice = Math.floor(Math.random() * 3);
-
-    if (intChoice == 0) {
-        return "Rock"
-    } else if (intChoice == 1) {
-        return "Paper"
-    } else if (intChoice == 2) {
-        return "Scissors"
-    }
+    return Math.floor(Math.random() * 3); // 0 = Rock, 1 = Paper, 2 = Scissors
 }
 
 function getHumanChoice() {
-    return prompt("Human! Choose!");
+    const choice = prompt("Human! Choose!").toLowerCase();
+
+    if (choice == "rock"){
+        return 0
+    } else if (choice == "paper") {
+        return 1
+    } else if (choice == "scissors") {
+        return 2
+    } else {
+        return 404
+    }
 }
 
-console.log(humanScore)
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        return "Tie!"
+    } else if ((humanChoice - computerChoice + 3) % 3 === 1) {
+        humanScore++
+        return "Human Wins!"
+    } else {
+        computerScore++
+        return "Computer Wins!"
+    }
+}
