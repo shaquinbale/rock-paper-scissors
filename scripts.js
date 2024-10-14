2bcc0ba3a2ea627aad71ee57c9beac5a1f50e62a
shaquinbale <scopelenskingdra@gmail.com>
2024-10-14 10:49:47 -0400

    Add randomized computer output
    Added a function to return a rock, paper, or
    scissors string randomly

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+function getComputerChoice() {
+    const intChoice = Math.floor(Math.random() * 3);
+
+    if (intChoice == 0) {
+        return "Rock"
+    } else if (intChoice == 1) {
+        return "Paper"
+    } else if (intChoice == 2) {
+        return "Scissors"
+    }
+}
+
+console.log(getComputerChoice())
